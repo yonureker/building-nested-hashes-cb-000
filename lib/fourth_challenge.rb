88f0882c11eaeb57aa0dee97096327bc2709ae94
Onur Eker <yonureker@gmail.com>
2017-12-25 23:43:17 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -17,7 +17,7 @@ def fourth_challenge
           :attitude => "hot-headed"
         }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -28,6 +28,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
